3c6de5f16bf8c4c477aed8fc957bd49284aa1d1f
celine-14 <leong_celine@outlook.com>
2022-07-10 22:22:17 +0000

    Add validation to check for exaclty 6 values in provided data

--- run.py
+++ run.py
@@ -12,6 +12,7 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('level_of_participants')
 
+
 def get_participants_data():
     """
     Get participants details from the user
@@ -21,6 +22,22 @@ def get_participants_data():
     print("Example: Jane, Doe, 4, 2, 1, 3\n")
 
     data_str = input("Enter your data here: ")
-    print(f"The data provided is {data_str}")
+
+    participants_data = data_str.split(",")
+    validate_data(participants_data)
+
+
+def validate_data(values):
+    """
+    Inside the try, raise ValueError if strings aren't exactly 6 values.
+    """
+    try:
+        if len(values) != 6:
+            raise ValueError(
+                f"Exaclty 6 values required, you provided {len(values)}"
+            )
+    except ValueError as e:
+        print(f"Invalid data: {e}, please try again.\n")
+
 
 get_participants_data()
