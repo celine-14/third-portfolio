3da576b3537c980ebf77cd272fb673da4c0e64c5
celine-14 <leong_celine@outlook.com>
2022-07-19 20:34:04 +0000

    Fix grammatical issues

--- run.py
+++ run.py
@@ -14,27 +14,30 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('level_of_participants')
 
 
-def get_participants_data():
+def get_participant_data():
     """
-    Get participants details from the user.
+    Get participant's details from the user.
     Run a while loop to collect a valid string of data from the user
     via the terminal, which must be a string of first name,
     last name, and 4 numbers separated by commas.
     The loop will repeatedly request data until it is valid.
     """
     while True:
-        print("Please enter participants data.")
-        print("Data should include first name, last name, piano level, \
-musicianship level, solfa level and conducting level, \
-separated by commas without spaces.")
+        print("Please enter participant's data.")
+        print(
+            "Data should include first name, last name, piano level,"
+            "musicianship level, solfa level and conducting level,"
+            "separated by commas without spaces."
+            )
         print("Example: Jane,Doe,4,2,1,3\n")
         data_str = input("Enter your data here: ")
-        participants_data = data_str.split(",")
+        participant_data = data_str.split(",")
+        participant_data = participant_data[:2] + [int(value) for value in participant_data[2:]]
 
-        if validate_data(participants_data):
+        if validate_data(participant_data):
             print("Data is valid!")
             break
-    return participants_data
+    return participant_data
 
 
 def validate_data(values):
@@ -44,31 +47,23 @@ def validate_data(values):
     Raise ValueError if last 4 string inputs cannot be converted into int,
     if there aren't exactly 6 values, if first and last names are not provided.
     """
-    try:
-        for value in values[:-4]:
-            if not value.isalpha():
-                raise ValueError(
-                    "First and Last name required. Names must be in alphabets"
-                    )
-            elif len(value) < 2:
-                raise ValueError(
-                    "Names must be more than 1 letter"
-                    )
-        [int(value) for value in values[2:]]
-        if len(values) != 6:
-            raise ValueError(
-                f"Exaclty 6 values required, you provided {len(values)}"
-            )
-    except ValueError as e:
-        print(f"Invalid data: {e}, please try again.\n")
-        return False
+    error_message = ""
+    for value in values[:2]:
+        if not value.isalpha():
+            error_message = "First and Last name required. Names must be in alphabets"
+        elif len(value) < 2:
+            error_message = "Names must be more than 1 letter"
+            
+    if len(values) != 6:
+        error_message = f"Exaclty 6 values required, you provided {len(values)}"
 
-    return True
+    print(error_message)
+    return error_message == ""
 
 
-def update_participants_data(data):
+def update_participant_data(data):
     """
-    Update participants worksheet, add new row with the list data provided.
+    Append the participant's worksheet to the data source
     """
     print("Updating participants worksheet...\n")
     participants_worksheet = SHEET.worksheet("participants")
@@ -76,9 +71,9 @@ def update_participants_data(data):
     print("Participants worksheet updated successfully.\n")
 
 
-def update_participants_data_levels(data):
+def update_participant_data_levels(data):
     """
-    Update levels worksheet, add new row with the list data provided.
+    Append the levels worksheet add new row with the names provided.
     """
     print("Updating levels worksheet...\n")
     levels_worksheet = SHEET.worksheet("levels")
@@ -86,13 +81,13 @@ def update_participants_data_levels(data):
     print("Levels worksheet updated successfully.\n")
 
 
-def calculate_average_level(participants_row):
+def calculate_average_level(participant_levels):
     """
     Calculate average music level for each paricipants
     """
     print("Calculating average level...\n")
     average_data = []
-    level_average = (sum(participants_row)) / 4
+    level_average = (sum(participant_levels)) / 4
     average_data.append(level_average)
 
     return average_data
@@ -102,15 +97,17 @@ def main():
     """
     Run all program functions
     """
-    data = get_participants_data()
-    participants_data = [name.capitalize() for name in data[:-4]] + [int(num) for num in data[2:]]
-    participants_names = [name.capitalize() for name in data[:-4]]
-    participants_level = [int(num) for num in data[2:]]
-    update_participants_data(participants_data)
-    update_participants_data_levels(participants_names)
-    new_average_level = calculate_average_level(participants_level)
+    data = get_participant_data()
+    participant_data = [name.capitalize() for name in data[:-4]] + [int(num) for num in data[2:]]
+    participant_names = [name.capitalize() for name in data[:-4]]
+    participant_levels = [int(num) for num in data[2:]]
+    update_participant_data(participant_data)
+    update_participant_data_levels(participant_names)
+    new_average_level = calculate_average_level(participant_levels)
     print(new_average_level)
 
 
 print("Welcome to Participants Data Automation")
-main()
+
+if __name__ == '__main__':
+    main()
